437d3a52cba43e095d4244287e8b99763764995a
Isa <isa.arbuhov@mail.ru>
2023-01-10 22:05:06 +0300

    Initial Commit

---
+++ HW/HWApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWApp.swift
+//  HW
+//
+//  Created by Иса on 10.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct HWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
